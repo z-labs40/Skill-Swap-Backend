b533e22686acdc5a790e8d599afef158569377b2
karthikeya-Dev-coder <karthikeya0pc@gmail.com>
2026-05-13 13:53:36 +0530

    quick fix: hardcode resend api key

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_aqkS6cXh_9rgoZPzBTyRWLBmyNJHzxmwr');
 
 interface EmailResponse {
   success: boolean;
